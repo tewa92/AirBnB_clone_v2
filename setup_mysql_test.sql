ecc946df5b1247023f9fc097a3a264eca0a6f981
tewa92 <tewachewsaleigizer@gmail.com>
2024-04-15 06:42:22 -0700

    mysql_test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,12 @@
--- To prepare the SERVER for the project
--- To create a DATABASE hbnb_dev_db and a new user hbnb_dev
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev' @'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev' @'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev' @'localhost';
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges on hbnb_test_db to hbnb_test
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_test
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
