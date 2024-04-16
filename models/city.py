097dbe8370673f3b5052a3c2f0a2151e281a1ca9
tewa92 <tewachewsaleigizer@gmail.com>
2024-04-16 06:14:53 -0700

    AirBnb_Clone_v2

--- models/city.py
+++ models/city.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import storage_type
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import relationship
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ The CITY class, contains STATE ID and NAME """
+    __table__ = 'cities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('place', backref='cities', cascade = 'all, delete, delete-orphan')
+    else:
+        name = ''
+        states_id = ''
--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models import storage_type
+from sqlalchemy import Column, String
+from sqlalchemy import relationship
+from models import storage
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """
+            Returns the list of CITY instances with STATE_ID
+            equals the current STATE.ID
+            FileStorage relationship between STATES AND CITY
+            """
+            related_cities = []
+            cities = storage.all(city)
+            for city in cities.value():
+                if city.state_id == self.id:
+                    related_cities.append(city)
+            return related_cities
