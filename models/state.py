4a7fbb62f893e22b3b3fb8989311b14aae8461cb
tewa92 <tewachewsaleigizer@gmail.com>
2024-04-16 07:30:04 -0700

    AirBnB_clone_v2

--- models/state.py
+++ models/state.py
@@ -12,22 +12,24 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state',
-                              cascade='all, delete, delete-orphan')
-    else:
+        # cities = relationship('City', backref='state',
+        # cascade='all, delete, delete-orphan')
+
+
+"""     else:
         name = ''
 
         @property
         def cities(self):
-            """
+
             Returns the list of CITY instances with STATE_ID
             equals the current STATE.ID
             FileStorage relationship between STATES AND CITY
-            """
+
             from models import storage
             related_cities = []
             cities = storage.all(City)
             for city in cities.values():
                 if city.state_id == self.id:
                     related_cities.append(city)
-            return related_cities
+            return related_cities """
